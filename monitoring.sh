5f6f0406d24b9c83f324b39eaf36a114296e4bc7
shinhwagk <shanghai_sl@hotmail.com>
2019-08-04 12:47:33 +0800

    Update monitoring.sh

--- monitoring.sh
+++ monitoring.sh
@@ -12,10 +12,7 @@ git log --since="${offset_date}" --pretty=format:"%h" | while read commit_id
 do
   git diff-tree --no-commit-id --name-only -r $commit_id | while read file
   do 
-    r=$(expr match $file $prefix)
-    if [[ $r != 0 ]]; then
-      echo $file
-    fi
+    if [[ "$(expr match $file $prefix)" != "0" ]]; then echo $file; fi
   done
 done
 echo "#################################################"
