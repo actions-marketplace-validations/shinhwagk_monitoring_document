ee11552628554c75941dfcea4b5251be1fc2d57f
shinhwagk <shanghai_sl@hotmail.com>
2019-08-04 10:15:08 +0800

    Update entrypoint.sh

--- .github/monitoring/entrypoint.sh
+++ .github/monitoring/entrypoint.sh
@@ -1,21 +1,23 @@
 #!/usr/bin/env bash
 
-git clone https://github.com/$1/$2
+# git clone https://github.com/$1/$2
 
-cd $2
+# cd $2
 
-git config --global log.date local
+# git config --global log.date local
 
-offset_date=`date -d "${3} hours ago" "+%Y-%m-%d %H:%M")`
-prefix=$4
+# offset_date=`date -d "${3} hours ago" "+%Y-%m-%d %H:%M")`
+# prefix=$4
 
-git log --since="${offset_date}" --pretty=format:"%h" | while read commit_id
-do
-  git diff-tree --no-commit-id --name-only -r $commit_id | while read file
-  do 
-    r=$(expr match $file $prefix)
-    if [[ $r != 0 ]]; then
-      echo $file
-    fi
-  done
-done
+# git log --since="${offset_date}" --pretty=format:"%h" | while read commit_id
+# do
+#   git diff-tree --no-commit-id --name-only -r $commit_id | while read file
+#   do 
+#     r=$(expr match $file $prefix)
+#     if [[ $r != 0 ]]; then
+#       echo $file
+#     fi
+#   done
+# done
+
+./monitoring $@
