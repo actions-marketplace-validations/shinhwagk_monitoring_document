5572181706de7d0d857729699ab28547e224bf6b
shinhwagk <shanghai_sl@hotmail.com>
2019-08-04 08:42:17 +0800

    Update monitoring.sh

--- monitoring.sh
+++ monitoring.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 git config --global log.date local
 
-curr_date=$(date -d "$(date) 1 hours ago" "+%Y-%m-%d %H:%M")
+offset_date=$(date -d "$(date) 1 hours ago" "+%Y-%m-%d %H:%M")
 prefix="content/en"
 
-git log --since="${curr_date}" --pretty=format:"%h" | while read commit_id
+git log --since="${offset_date}" --pretty=format:"%h" | while read commit_id
 do
   git diff-tree --no-commit-id --name-only -r $commit_id | while read file
   do 
