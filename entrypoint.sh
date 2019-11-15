eb5328479abdb64f8876f695cfd368d8262570de
shinhwagk <shanghai_sl@hotmail.com>
2019-11-15 11:14:41 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,5 +20,23 @@
 #   done
 # done
 
-./monitoring.sh $@
-ls -l ./
+repo_url=https://github.com/${owner}/${repo}
+git clone ${repo_url} && cd ${repo}
+
+git config --global log.date local
+
+offset_date=$(date -d "${offset_hours} hours ago" "+%Y-%m-%d %H:%M")
+prefix=${prefix}
+
+echo "#################################################"
+git log --since="${offset_date}" --pretty=format:"%h" | while read commit_id
+do
+  git diff-tree --no-commit-id --name-only -r $commit_id | while read file
+  do 
+    if [[ "$(expr match $file $prefix)" != "0" ]]; then
+      curl -X POST -H 'Content-type: application/json' \
+      --data "{\"text\":\"${repo_url}/blob/master/$file\"}" $incoming_webhooks
+    fi
+  done
+done
+echo "#################################################"
