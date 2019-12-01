f01bf19dab3b01ecb16888f3109d4ed3666ac74c
shinhwagk <shanghai_sl@hotmail.com>
2019-12-01 15:18:50 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,27 +1,5 @@
 #!/bin/sh -l
 
-# git clone https://github.com/$1/$2
-
-# cd $2
-
-# git config --global log.date local
-
-# offset_date=`date -d "${3} hours ago" "+%Y-%m-%d %H:%M")`
-# prefix=$4
-
-# git log --since="${offset_date}" --pretty=format:"%h" | while read commit_id
-# do
-#   git diff-tree --no-commit-id --name-only -r $commit_id | while read file
-#   do 
-#     r=$(expr match $file $prefix)
-#     if [[ $r != 0 ]]; then
-#       echo $file
-#     fi
-#   done
-# done
-
-echo $incoming_webhooks
-
 repo_url=https://github.com/${owner}/${repo} && echo "monitoring repo: $repo_url"
 git clone ${repo_url} && cd ${repo}
 
